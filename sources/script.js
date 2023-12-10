63fa7fe2679ba8f8bc5d44eb0c929ce19cd947ac
iwenki <139893979+iwenki@users.noreply.github.com>
2023-12-10 09:03:11 -0600

    Added script and typewriter plug-in

--- sources/script.js
+++ sources/script.js
@@ -1,1 +1,13 @@
-alert(`Hello`);
+let generatorElement=document.querySelector("#form-container");
+generatorElement.addEventListener("submit", generateQuote);
+
+function generateQuote(event){
+  event.preventDefault();
+
+new Typewriter("#quote", {
+  strings: "It does not do to dwell on dreams and forget to live.",
+  autoStart: true,
+  cursor: "",
+  delay: 160,
+});
+}
