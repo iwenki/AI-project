cffe877a73ad707f331ede2c8b59905f2d591b5e
iwenki <139893979+iwenki@users.noreply.github.com>
2023-12-10 12:17:38 -0600

    Added axios and api information for AI generator

--- sources/script.js
+++ sources/script.js
@@ -3,11 +3,22 @@ generatorElement.addEventListener("submit", generateQuote);
 
 function generateQuote(event){
   event.preventDefault();
+  let userFeedback=document.querySelector(".text-box");
+  let apiKey = "cbc90ba0a21t28a990f44b7f6f3ea68o";
+  let context =
+    "You are an AI assistant that has read the Harry Potter series by J.K Rowling, The Alchemist by Paulo Coelho, The Lord of the Rings by John Ronald Reuel Tolkien and A Song of Ice and Fire series by George R. R. Martin. You have collected a wide variety of your favorite and meaningful quotes from these books.";
+let prompt= `Please provide a short quote about ${userFeedback.value}.At the end of the quote, state the character, book and author from where the quote comes from.`;
+let apiUrl =
+  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayQuote);
 
-new Typewriter("#quote", {
-  strings: "It does not do to dwell on dreams and forget to live.",
+
+}
+function displayQuote(response){
+  new Typewriter("#quote", {
+  strings: response.data.answer,
   autoStart: true,
   cursor: "",
-  delay: 160,
+  delay: 80,
 });
 }
